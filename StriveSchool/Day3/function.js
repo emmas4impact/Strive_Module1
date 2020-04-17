6f1afd635c24d6e63f2d724bc29253dc81f5cd90
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-04-17 13:44:22 +0300

    func updates

--- StriveSchool/Day3/function.js
+++ StriveSchool/Day3/function.js
@@ -2,6 +2,7 @@
 
 let number = 4
 const squareFunction = function(num){
+    console.log
     const square = num * num
 
     return square
@@ -16,3 +17,15 @@ const sumResult = function(a,b){
     return a+b
 }
  console.log('Sum Result: '+ sumResult(4,6))
+
+ let val = 10
+
+ if(val<=10){
+     y = 15
+     console.log('Add Value Y to declared Val: '+sumResult(val,y))
+
+ }else{
+     console.log('Add Value 0 to declared Val: '+sumResult(val,0))
+ }
+
+ const squares = x => x*x
