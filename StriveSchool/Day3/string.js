5ae1795e0a16d66866812f4346fd6140f111a68b
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-04-17 12:04:30 +0300

    update

--- StriveSchool/Day3/string.js
+++ StriveSchool/Day3/string.js
@@ -7,3 +7,17 @@ Second line
 The is my value ${2 +3}
 `
 console.log(myString)
+
+const myString2 = 'whatever'
+myString2.slice(3,6)
+myString2.indexOf('a')
+myString2.trim()
+
+const sentence = `Hello strivers we are trying split method`
+const splittedwords = sentence.split(" ")
+
+console.log(splittedwords)
+
+console.log(splittedwords.join(',       '))
+
+console.log("asd".repeat(5))
